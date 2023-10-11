ade5d48ad598c29a912ac1cc9d4b59568b2c1289
kaviarasu-thuraiarasu <kaviarasu.thuraiarasu@gmail.com>
2023-10-11 12:41:47 +0530

    First commit

--- App.js
+++ App.js
@@ -17,12 +17,13 @@
 //   "Hello World from React!!!"
 // );
 // { id: "title", name: "tet"} ==> This is the place to add the attribute to the element!!
-
+import React from "react";  
+import ReactDOM from "react-dom/client"                                  
 const element = React.createElement(
   "div",
   { id: "parent" },
   [React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Nuverlan Technologies"),
+    React.createElement("h1", {}, "Nuverlan Technologies cdfdfdfd"),
     React.createElement("h1", {}, "Nuverlan Academy"),
   ]),
   React.createElement("div", { id: "child2" }, [
