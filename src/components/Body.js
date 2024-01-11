4bb1965e13874c71674e40df7b35e18af820d851
Your Name <you@example.com>
2024-01-11 11:13:40 +0530

    API Changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -50,11 +50,12 @@ const [search,setSearch] = useState("")
 const fetchData = async ()=>{
   const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
   const json = await data.json()
-  setinitialState(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+  console.log("==",json);
+  setinitialState(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
     ?.restaurants)
-  setListData(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+  setListData(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
 ?.restaurants)
-console.log(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle
+console.log(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
   ?.restaurants)
 }
 // console.log(list_data)
